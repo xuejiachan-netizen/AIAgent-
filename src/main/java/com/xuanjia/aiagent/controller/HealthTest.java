a78a8fc847cbd686e5e008e17ef32d598ee896e3
chenxuanjia <chenxuanjia@>
2025-11-10 01:05:35 +0800

    AI 接入

---
+++ src/main/java/com/xuanjia/aiagent/controller/HealthTest.java
@@ -0,0 +1,16 @@
+package com.xuanjia.aiagent.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthTest {
+
+    @GetMapping("/test")
+    public String test(){
+        return "Hello!";
+    }
+}
